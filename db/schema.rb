1f20c71f4f24d9bcd4f84182ece7f1f9efd54ac9
Martin-Alexander <martingianna@gmail.com>
2020-03-31 02:25:15 -0400

    added 'next round' logic inside game model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_003916) do
+ActiveRecord::Schema.define(version: 2020_03_31_035446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 2020_03_31_003916) do
     t.boolean "started"
     t.boolean "finished"
     t.integer "current_user"
+    t.integer "seconds_left", default: 60
     t.index ["user_id"], name: "index_games_on_user_id"
   end
 

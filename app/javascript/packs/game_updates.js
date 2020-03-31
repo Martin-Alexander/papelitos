0b15cfea69c2278e127300d3cd3d7e64c2ee500f
Martin-Alexander <martingianna@gmail.com>
2020-03-31 02:29:16 -0400

    refactored a bit the handling of information on the front-end

--- app/javascript/packs/game_updates.js
+++ app/javascript/packs/game_updates.js
@@ -1,54 +1,83 @@
 import { csrfToken } from '@rails/ujs';
+
 const increaseScrore = document.getElementById('increase-score');
-const currentWord = document.querySelector('#current-word');
-const teamPlaying = document.querySelector('#team-playing');
-const youArePlaying = document.querySelector('#you-are-playing');
-const whoIsPlaying = document.querySelector('#who-is-playing');
-const secondsLeft = document.querySelector('#seconds-left');
-const teamOnePoints = document.querySelector('#team-1-points');
-const teamTwoPoints = document.querySelector('#team-2-points');
-const roundName = document.querySelector('#round-name');
-const gameState = document.getElementById('game-state');
-const gameId = gameState.dataset.gameId;
-const gameTeamId = gameState.dataset.gameTeam;
-const gameWords = JSON.parse(gameState.dataset.gameWords);
-console.log(Array.isArray(gameWords));
-if (currentWord) currentWord.innerHTML = gameWords.pop();
-setInterval(() => {
-  fetch(`/games/${gameId}`, {
-    method : 'get',
-    headers: {
-      'X-CSRF-Token': csrfToken(),
-      credentials   : 'same-origin',
-      Accept        : 'application/json'
-    }
-  })
+const currentWord    = document.querySelector('#current-word');
+const teamPlaying    = document.querySelector('#team-playing');
+const whoIsPlaying   = document.querySelector('#who-is-playing');
+const activePlayer   = document.querySelector('#active-player');
+const inactivePlayer = document.querySelector('#inactive-player');
+const secondsLeft    = document.querySelector('#seconds-left');
+const teamOnePoints  = document.querySelector('#team-1-points');
+const teamTwoPoints  = document.querySelector('#team-2-points');
+const roundName      = document.querySelector('#round-name');
+const gameState      = document.getElementById('game-state');
+
+if (gameState) {
+  const gameId = gameState.dataset.gameId;
+  const gameTeamId = gameState.dataset.gameTeam;
+  let gameWords = JSON.parse(gameState.dataset.gameWords);
+
+  setInterval(() => {
+    fetch(`/games/${gameId}`, {
+      method : 'get',
+      headers: {
+        'X-CSRF-Token': csrfToken(),
+        credentials   : 'same-origin',
+        Accept        : 'application/json'
+      }
+    })
     .then(response => response.json())
     .then((data) => {
       if (teamPlaying) teamPlaying.innerHTML = data.team_playing;
-      if (youArePlaying) youArePlaying.innerHTML = data.you_are_playing;
-      if (secondsLeft) secondsLeft.innerHTML = data.seconds_left;
-      if (teamOnePoints) teamOnePoints.innerHTML = data.team_1_points;
-      if (teamTwoPoints) teamTwoPoints.innerHTML = data.team_2_points;
-      if (roundName) roundName.innerHTML = data.round_name;
-      if (whoIsPlaying) whoIsPlaying.innerHTML = data.who_is_playing;
+
+      if (data.you_are_playing) {
+        activePlayer.style.display = "initial";
+        inactivePlayer.style.display = "none";
+      } else {
+        activePlayer.style.display = "none";
+        inactivePlayer.style.display = "initial";
+      }
+
+      secondsLeft.innerHTML = data.seconds_left;
+      teamOnePoints.innerHTML = data.team_1_points;
+      teamTwoPoints.innerHTML = data.team_2_points;
+      roundName.innerHTML = data.round_name;
+      whoIsPlaying.innerHTML = data.who_is_playing;
     });
-}, 1000);
-if (increaseScrore) {
-  increaseScrore.addEventListener('click', () => {
-    if (gameWords.length > 0) {
-      currentWord.innerHTML = gameWords.pop();
-    } else {
-      currentWord.innerHTML = 'Finish!';
-    }
-    fetch(`/game_teams/${gameTeamId}`, {
-      method : 'PATCH',
-      headers: {
-        'X-CSRF_TOKEN': csrfToken(),
-        credentials   : 'same-origin',
-        Accept        : 'application/json'
-      },
-      body: JSON.stringify({})
+  }, 1000);
+
+  currentWord.innerHTML = gameWords[0];
+
+  if (increaseScrore) {
+    increaseScrore.addEventListener('click', () => {
+      if (gameWords.length > 0) {
+        const completedWord = gameWords.shift();
+        const nextWord = gameWords[0];
+        
+        fetch(`/game_teams/${gameTeamId}`, {
+          method : 'PATCH',
+          headers: {
+            'X-CSRF_TOKEN': csrfToken(),
+            credentials   : 'same-origin',
+            Accept        : 'application/json',
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            roundOver: nextWord === undefined,
+            completedWord: completedWord
+          })
+        });
+
+        if (nextWord) {
+          currentWord.innerHTML = nextWord;
+        } else {
+          activePlayer.style.display = "none";
+          inactivePlayer.style.display = "initial";
+          gameWords = JSON.parse(gameState.dataset.gameWords);
+          currentWord.innerHTML = gameWords[0];
+        }
+      }
     });
-  });
+  }
 }
+

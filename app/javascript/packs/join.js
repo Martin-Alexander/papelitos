1e756263c02f72630fd22da7ada7197041be66c5
Martin-Alexander <martingianna@gmail.com>
2020-03-31 02:29:47 -0400

    commented out and guarded some more JS

--- app/javascript/packs/join.js
+++ app/javascript/packs/join.js
@@ -1,9 +1,11 @@
 const join = document.querySelector(".join-button");
+const form = document.querySelector(".join-form");
 
+if (form) {
+  join.addEventListener("click", (event) => {
+    form.classList.remove("hide-form");
+  });
+}
 
-join.addEventListener("click", (event) => {
-  const form = document.querySelector(".join-form");
-  form.classList.remove("hide-form");
-});
 
 
